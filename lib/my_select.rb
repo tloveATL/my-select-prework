0fe12509dc7fa0dc09064cd82f172c1f52b3bb61
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-15 15:38:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,10 @@
 def my_select(collection)
- i = 0
- selections = []
- while collection.length > 0
-  if yield(collection[i]) == true
-    selections << collection[i]
-  end
-  i += 1
-end 
+  i = 0
+  selections = []
+  while collection.length > 0
+    if yield(collection[i]) == true
+      selections << collection[i]
+    end
+    i += 1
+  end 
 end
